8e71e883cac978e5b22742817e4afb5dfc3c7506
Nikodem Brożyniak <nbrozyniak@gmail.com>
2023-11-16 14:11:34 +0100

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/titles",
     createProxyMiddleware({
-      target: "http://127.0.0.1:5000",
+      target: "https://niki3d.pythonanywhere.com/",
       changeOrigin: true,
     })
   );
