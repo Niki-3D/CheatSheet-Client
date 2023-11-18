1eaa0054ffb6508c1c44eebc6b888d5e2fdfd7ed
Nikodem Bożyniak <116508040+Niki-3D@users.noreply.github.com>
2023-11-18 15:30:05 +0100

    a

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    "/titles",
-    createProxyMiddleware({
-      target: "https://niki3d.pythonanywhere.com/",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     "/titles",
+//     createProxyMiddleware({
+//       target: "https://niki3d.pythonanywhere.com/",
+//       changeOrigin: true,
+//     })
+//   );
+// };
